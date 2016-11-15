2bb3e2201d9ccfc18dfd40eb6a571539181aecde
Rbobbo <riccardo.bobbo@gmail.com>
2016-11-15 12:38:52 +0100

    Edit custom test

--- src/main/java/it/cg/main/integration/easyway/chain/CustomTest.java
+++ src/main/java/it/cg/main/integration/easyway/chain/CustomTest.java
@@ -1,9 +1,7 @@
 package it.cg.main.integration.easyway.chain;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.List;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
@@ -32,17 +30,12 @@ public class CustomTest
 	
 	private void getWsCalculatePremiumInput(CalculatePremium cp)
 	{
-		TypeBooleano tyBool = new TypeBooleano();
-		tyBool.setBoolean(true);
-		cp.getArg0().getInput().setQuoteMode(tyBool);
-		tyBool = new TypeBooleano();
-		tyBool.setBoolean(true);
-		cp.getArg0().getInput().setAdaptToMinimumPremium(tyBool);
-		tyBool = new TypeBooleano();
-		tyBool.setBoolean(true);
-		cp.getArg0().getInput().setApplyDiscount(tyBool);
-		tyBool = new TypeBooleano();
-		tyBool.setBoolean(true);
+		tyBoolGen.setBoolean(false); cp.getArg0().getInput().setQuoteMode(tyBoolGen);
+		
+		tyBoolGen = new TypeBooleano(); tyBoolGen.setBoolean(true);
+		cp.getArg0().getInput().setAdaptToMinimumPremium(tyBoolGen);
+		tyBoolGen = new TypeBooleano(); tyBoolGen.setBoolean(true);
+		cp.getArg0().getInput().setApplyDiscount(tyBoolGen);
 	}
 	
 	private void getWsProduct(CalculatePremium cp) throws DatatypeConfigurationException
@@ -109,49 +102,44 @@ public class CustomTest
 	private void getFactorsWsUnitInstance(WsUnitInstance unitInstance)
 	{
 		WsFactor factorTest  ;
-		factorTest = new WsFactor();factorTest.setCode("3MASS");factorTest.setValue("1");unitInstance.getFactors() .add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("3RCFRA");factorTest.setValue("1");unitInstance.getFactors() .add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("3YDAGE");factorTest.setValue("1");unitInstance.getFactors() .add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("SXTOT");factorTest.setValue("6");unitInstance.getFactors() .add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("FRRCA");factorTest.setValue("6");unitInstance.getFactors() .add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("QDRCA");factorTest.setValue("6");unitInstance.getFactors() .add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("3MASS");factorTest.setValue("1");unitInstance.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("3RCFRA");factorTest.setValue("1");unitInstance.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("SXTOT");factorTest.setValue("0.0");unitInstance.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("FRRCA");factorTest.setValue("1.0");unitInstance.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("QDRCA");factorTest.setValue("1.0");unitInstance.getFactors().add(factorTest);
 	}
 	
 	private void getFactorsAssetInstances(WsAssetInstance assetInstances)
 	{
 		WsFactor factorTest  ;
 		factorTest = new WsFactor();factorTest.setCode("2USOVE");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2CLUWR");factorTest.setValue("4");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("1PHAP");factorTest.setValue("41");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("1ANRIN");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("1AFF");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2PETA");factorTest.setValue("8");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2PRCA");factorTest.setValue("8");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2CETA");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2CRCA");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2CLUWR");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("1PHAP");factorTest.setValue("-1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("1ANRIN");factorTest.setValue("0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("1AFF");factorTest.setValue("0.0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2PETA");factorTest.setValue("18");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2PRCA");factorTest.setValue("11");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2CETA");factorTest.setValue("18");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2CRCA");factorTest.setValue("11");assetInstances.getFactors().add(factorTest);
 		factorTest = new WsFactor();factorTest.setCode("2MDPR");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2MDAP");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2RD1ET");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2RD1CA");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2NDRIV");factorTest.setValue("4");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2ETAV");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2EVACQ");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2ANUCF");factorTest.setValue("4");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2ALAUT");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2DICH");factorTest.setValue("4");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("3CLIN1");factorTest.setValue("3");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2FTAP");factorTest.setValue("3");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2MDAP");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2RD1ET");factorTest.setValue("-1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2RD1CA");factorTest.setValue("-1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2NDRIV");factorTest.setValue("1.0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2ETAV");factorTest.setValue("0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2EVACQ");factorTest.setValue("0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2ANUCF");factorTest.setValue("1.0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2ALAUT");factorTest.setValue("Sì");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("3CLIN1");factorTest.setValue("Sì");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2FTAP");factorTest.setValue("7");assetInstances.getFactors().add(factorTest);
 		factorTest = new WsFactor();factorTest.setCode("_2CUPR");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2CUAS");factorTest.setValue("18");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("_2CU");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2OWAP");factorTest.setValue("4");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2WCAP");factorTest.setValue("3");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2BERSP");factorTest.setValue("2");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("1FIDRC");factorTest.setValue("41");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2ASSIN");factorTest.setValue("4");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("2SIN6P");factorTest.setValue("101");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("1OPER");factorTest.setValue("41");assetInstances.getFactors().add(factorTest);
-		factorTest = new WsFactor();factorTest.setCode("1FRRIN");factorTest.setValue("41");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2CUAS");factorTest.setValue("1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("_2CU");factorTest.setValue("1B");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2OWAP");factorTest.setValue("-1");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2BERSP");factorTest.setValue("Sì");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("1FIDRC");factorTest.setValue("1.0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("2ASSIN");factorTest.setValue("0.0");assetInstances.getFactors().add(factorTest);
+		factorTest = new WsFactor();factorTest.setCode("1FRRIN");factorTest.setValue("0.0");assetInstances.getFactors().add(factorTest);
 	}
 	
 	private void getFactorsWsProduct(WsProduct wsProduct)
@@ -159,8 +147,8 @@ public class CustomTest
 		WsFactor factorTest;
 		factorTest = new WsFactor();factorTest.setCode("_1CETA");factorTest.setValue("18");wsProduct.getFactors().add(factorTest );
 		factorTest = new WsFactor();factorTest.setCode("_1CNCA");factorTest.setValue("11");wsProduct.getFactors().add(factorTest );
-		factorTest = new WsFactor();factorTest.setCode("_2BM");factorTest.setValue("3");wsProduct.getFactors().add(factorTest );
-		factorTest = new WsFactor();factorTest.setCode("_2AAVE");factorTest.setValue("Da altro Veicolo del proprietario");wsProduct.getFactors().add(factorTest );
+		factorTest = new WsFactor();factorTest.setCode("_2BM");factorTest.setValue("1");wsProduct.getFactors().add(factorTest );
+		factorTest = new WsFactor();factorTest.setCode("_2AAVE");factorTest.setValue("1");wsProduct.getFactors().add(factorTest );
 	}
 
 	/***
